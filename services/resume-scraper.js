a0b8ebee39830b91236f2e66d67ba7d6f7153dea
Laura Tebben <tebbenla@mail.uc.edu>
2017-02-03 21:02:24 -0500

    beginnings

---
+++ services/resume-scraper.js
@@ -0,0 +1,34 @@
+var LOG = require("C:/Users/ltebb/node_modules/pdfreader/lib/LOG.js").toggle(false);
+var PdfReader = require("C:/Users/ltebb/node_modules/pdfreader/index.js").PdfReader;
+var fs = require('fs');
+
+
+function printRawItems(filename, callback){
+ var stream = new PdfReader().parseFileItems(filename, function(err, item){
+    if (err)
+      callback(err);
+    else if (!item)
+      callback();
+    else if (item.file)
+      console.log("file =", item.file.path);
+    else if (item.page)
+      console.log("page =", item.page);
+    else if (item.x)
+      stream.pipe(fs.createWriteStream("C:/Users/ltebb/workdir/recruiter-without-the-e/services/output.txt", item.text, function(er){
+          if(er){return console.log(er);}
+      }));
+    else
+      console.warn(item);
+  });
+}
+
+var filename = process.argv[2];
+if (!filename) {
+  console.error("please provide the name of a PDF file");
+}
+else {
+  console.warn("printing raw items from file:", filename, "...");
+  printRawItems(filename, function(){
+    console.warn("done.");
+  });
+}
