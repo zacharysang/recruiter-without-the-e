7e78b6f888b56e4b638d3566210927713a6c7361
Laura Tebben <tebbenla@mail.uc.edu>
2017-02-03 23:50:48 -0500

    buzzwords

--- services/resume-scraper.js
+++ services/resume-scraper.js
@@ -15,8 +15,15 @@ function printRawItems(filename, callback){
       console.log("page =", item.page);
     else if (item.x)
      {
-        var logStream = fs.createWriteStream("C:/Users/ltebb/workdir/recruiter-without-the-e/services/output.txt", {'flags': 'a'});
-        logStream.write(" " + item.text);
+        var logStream = fs.createWriteStream("C:/Users/ltebb/workdir/recruiter-without-the-e/services/output.txt");
+        fs.appendFile('output.txt', "\n" + item.text, function(er){
+          callback(er);
+        });
+/*        var ed = [item.text].find('Education');
+        console.log(ed);
+        if(ed){
+          console.log("Help");
+        }*/
      }
     else
       console.warn(item);
@@ -33,3 +40,33 @@ else {
     console.warn("done.");
   });
 }
+
+/*var f = require('fs');
+var word = "Education";
+f.readFile("output.txt", function(e, cont){
+  var re = new RegExp('^.*' + word + '.*$', 'm');
+  var m = re.exec(cont);
+  if (m)
+    console.log('Word %j found on line: %j', word, m[0]);
+  else
+    console.log('Word %j not found', word);
+});*/
+var JFile=require('jfile');
+var txtFile=new JFile("output.txt");
+var buzzwords = ["c++", "java", "javascript", " iot ", "cloud", "parallel computing", " ai ", "artificial intelligence", "html", "css", "deep learning", "machine learning", "python"];
+for(let word of buzzwords){
+  var result = txtFile.grep(word);
+  if(result.length>0) console.log(word);
+}
+/*var f = require('fs');
+var s = fs.createReadStream("output.txt");
+var found = false;
+s.on('data', function(d){
+  if(!found) found=!!('' + d).match("Education");
+});
+s.on('error', function(e){
+  then(e, found);
+});
+s.on('close', function(e){
+  then(e, found);
+});*/
