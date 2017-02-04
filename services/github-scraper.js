e8a411bd62f7b735b548ddb57f3695338013d787
Zach Collins <collinzy@mail.uc.edu>
2017-02-03 21:16:59 -0500

    why is it not working...

--- services/github-scraper.js
+++ services/github-scraper.js
@@ -1,16 +1,9 @@
 var GitHub = require('github-api');
-var Contributions = require('github-user-contributions');
-
-var candidateGitHub = new Object();
 
 var gh = new GitHub();
 
 var user = gh.getUser('zcollins0');
 
 user.getProfile(function(err, person) {
-    candidateGitHub.name = person.name;
-    candidateGitHub.company = person.company;
-    candidateGitHub.bio = person.bio;
-    candidateGitHub.public_repo_count = person.public_repos;
-    console.log(candidateGitHub);
+    console.log(person);
 });
