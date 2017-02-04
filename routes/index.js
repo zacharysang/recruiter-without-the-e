e523c98751a8da5ead0851cd7e721f81ff7775b7
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-02-04 00:58:54 -0500

    Architect routing for application

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 var users = require('./users');
+var ideal = require('./ideal')
+var lookup = require('./lookup')
 var callbacks = require('./callbacks');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/ideal',function(req,res,next){
-	res.render('ideal',{title: 'Express'});
-});
+router.use('/ideal', ideal)
+
+router.use('/lookup', lookup)
 
 router.use('/callback', callbacks);
 
