2a51fd6ecef7163b03a70af1fdd8d002bcbdc32e
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-02-04 15:03:58 -0500

    Create sessions

--- controllers/ideal.js
+++ controllers/ideal.js
@@ -7,6 +7,16 @@ exports.renderForm = function(req, res) {
     res.render('ideal');
 }
 
+exports.postIdeal = function(req, res) {
+    var gpa = req.body.GPA;
+    var experience = req.body.experience;
+    var num_companies = req.body.num_companies;
+    var commit_count = req.body.commit_count;
+    
+    req.session.IdealCandidate = {profile: {gpa: gpa, experience: experience, num_companies: num_companies, commit_count: commit_count}};
+    res.redirect('lookup');
+}
+
 exports.compare = function(ideal, candidate) {
     Promise.resolve(candidate).then(function(x) {
         var resultlangs = [];
