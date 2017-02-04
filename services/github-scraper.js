80fafca46a8a38d6b7654e2bbd0b03dde7fc1172
Zach Collins <collinzy@mail.uc.edu>
2017-02-03 20:30:12 -0500

    make it a function

--- services/github-scraper.js
+++ services/github-scraper.js
@@ -1,20 +1,24 @@
 var GitHub = require('github-api');
 var Contributions = require('github-user-contributions');
 
-var candidateGitHub = new Object();
+function analyzeGitHub() {
+    var candidateGitHub = new Object();
 
-var gh = new GitHub();
+    var gh = new GitHub();
 
-var user = gh.getUser('zcollins0');
+    var user = gh.getUser('zcollins0');
 
-user.listRepos(function(err, repos) {
-    //console.log(repos);
-});
+    user.listRepos(function(err, repos) {
+        //console.log(repos);
+    });
 
-user.getProfile(function(err, person) {
-    candidateGitHub.name = person.name;
-    candidateGitHub.company = person.company;
-    candidateGitHub.bio = person.bio;
-    candidateGitHub.public_repo_count = person.public_repos;
-    console.log(candidateGitHub);
-});
+    user.getProfile(function(err, person) {
+        candidateGitHub.name = person.name;
+        candidateGitHub.company = person.company;
+        candidateGitHub.bio = person.bio;
+        candidateGitHub.public_repo_count = person.public_repos;
+        console.log(candidateGitHub);
+    });
+}
+
+analyzeGitHub();
